60638d76b8861e46134cb968c29e930c306d9802
Festo1 <festoeducation@gmail.com>
2023-10-14 17:03:42 +0300

    tests/

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -14,7 +14,6 @@ from time import sleep
 from models.amenity import Amenity
 
 
-# Define a test class for testing the instantiation of the Amenity class
 class TestAmenity_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Amenity class."""
 
@@ -86,7 +85,6 @@ class TestAmenity_instantiation(unittest.TestCase):
             Amenity(id=None, created_at=None, updated_at=None)
 
 
-# Define a test class for testing the save method of the Amenity class
 class TestAmenity_save(unittest.TestCase):
     """Unittests for testing save method of the Amenity class."""
 
@@ -138,7 +136,6 @@ class TestAmenity_save(unittest.TestCase):
             self.assertIn(amid, f.read())
 
 
-# Define a test class for testing the to_dict method of the Amenity class
 class TestAmenity_to_dict(unittest.TestCase):
     """Unittests for testing to_dict method of the Amenity class."""
 
